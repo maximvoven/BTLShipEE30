5612fb68c44ca3dec29f53153062e877ecfe73db
Maxim Vovenko <33429176+maximvoven@users.noreply.github.com>
2020-11-18 16:48:14 -0800

    Fixed AI Stages 1-2

--- Eclipse Workspace/Battleship/Battleship/ai.cpp
+++ Eclipse Workspace/Battleship/Battleship/ai.cpp
@@ -29,6 +29,8 @@ void easyAiPlayer(int out[2]){
 
 //todo optimize code to have method calls?.
 void aiPlayer(board Board, aiMemory *memory,int shot[2]){
+	shot[0]=-1;
+	shot[1]=-1;
 	//Reset Ai Engine State if we sink ship
 	if(Board.ShipSunk){
 		Board.ShipSunk=false;
@@ -78,15 +80,12 @@ void aiPlayer(board Board, aiMemory *memory,int shot[2]){
 	if(Board.ShotHit && memory->searchInProgress==1){
 		//If Last Shot Hit Keep Shooting in that direction
 		memory->searchInProgress=2;
+		memory->secondShot=memory->firstShot;
 	}else if(!Board.ShotHit && memory->searchInProgress==1){
 		//If Shot Miss Continue + Search Pattern
 		memory->searchState++;
 		do{
 			switch(memory->searchState){
-			case 1:
-				shot[0]=memory->secondShot[0]-1;
-				shot[1]=memory->secondShot[1];
-				break;
 			case 2:
 				shot[0]=memory->secondShot[0]+1;
 				shot[1]=memory->secondShot[1];
@@ -99,15 +98,20 @@ void aiPlayer(board Board, aiMemory *memory,int shot[2]){
 				shot[0]=memory->secondShot[0];
 				shot[1]=memory->secondShot[1]+1;
 				break;
+			default:
+				perror("Search State Exceeded 4, Progress State 1");
+				exit(EXIT_FAILURE);
+				break;
 			}
-			if(!(isValidRC(shot[0],shot[1])&&isEmpty(shot[0],shot[1],Board))){
+			if(!(isValidRC(shot[0],shot[1]) || isEmpty(shot[0],shot[1],Board))){
 				memory->searchState=(memory->searchState)+1;
-				if((memory->searchState) > 4){
-					resetMemory(memory);
-					aiPlayer(Board, memory, shot);
-				}
 			}
-		}while(isValidRC(shot[0],shot[1])&&isEmpty(shot[0],shot[1],Board));
+			if((memory->searchState) > 4){
+				resetMemory(memory);
+				aiPlayer(Board, memory, shot);
+				break;
+			}
+		}while(!isValidRC(shot[0],shot[1]) || !isEmpty(shot[0],shot[1],Board));
 		memory->firstShot[0]=shot[0];
 		memory->firstShot[1]=shot[1];
 		return;
@@ -141,10 +145,8 @@ void aiPlayer(board Board, aiMemory *memory,int shot[2]){
 		memory->searchInProgress=3;
 	}
 
-
-
-
 }
+
 static void resetMemory(aiMemory *memory){
 	memory->firstShot[0]=-1;
 	memory->firstShot[1]=-1;
@@ -161,8 +163,8 @@ static bool isEmpty(int r, int c, board Board){
 }
 
 static bool isValidRC(int r,int c){
-	if(r>=0 && r<10){
-		if(c>=0 &&c<10){
+	if((r>=0) && (r<=9)){
+		if((c>=0) && (c<=9)){
 			return true;
 		}
 	}
--- Eclipse Workspace/Battleship/Battleship/battleship.cpp
+++ Eclipse Workspace/Battleship/Battleship/battleship.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <ctype.h>
+#include <math.h>
 
 #include "battleship.h"
 #include "randomShip.h"
@@ -21,6 +22,9 @@
 static void debugState(board Board, aiMemory ai);
 static bool isValidRC(int r,int c);
 static bool isHit(int board[10][10], int r, int c);
+static void shipSunk(board *player1, board *player2, int shot[2]);
+static int sGameOver(int arr[10][10]);
+static bool isGameOver(int arr[10][10],int gameOverState);
 
 void launchBattlehip(){
 	setbuf(stdout,NULL);
@@ -35,8 +39,11 @@ void launchBattlehip(){
 	setupShipsR(player1.board);
 	setupShipsR(player2.board);
 
+	int gameOverState1 = sGameOver(player1.board);
+	int gameOverState2 = sGameOver(player2.board);
+
 	while(!gameEnd){
-		//Player Code
+
 
 		clearScreen();
 		displayShots(player1.shots);
@@ -48,12 +55,15 @@ void launchBattlehip(){
 		}
 		//Ai Vs Human Player, Selected by game mode
 		if(gameMode>=1){
-//			printf("Enter Firing Coordinates (Letter Number):\n");
-//			do{
-//				char r;
-//				scanf("%c%d", &r,&shot[1]);
-//				shot[0]=tolower(r)-97;
-//			}while(!isValidRC(shot[0], shot[1]));
+			printf("Enter Firing Coordinates (Letter Number):\n");
+			do{
+				char r;
+				scanf("%c%d", &r,&shot[1]);
+				shot[0]=tolower(r)-97;
+				if(!isValidRC(shot[0], shot[1])){
+					fputs("Coordinates Invalid\n",stdout);
+				}
+			}while(!isValidRC(shot[0], shot[1]));
 		} else {
 			printf("Ai 1 Is Thinking ... ");
 			aiPlayer(player1, &ai1, shot);
@@ -69,16 +79,32 @@ void launchBattlehip(){
 			player1.shots[shot[0]][shot[1]]=miss;
 			player1.ShotHit=false;
 		}
-
+		shipSunk(&player1, &player2, shot);
+		if((gameEnd=isGameOver(player2.board, gameOverState2))){
+			printf("Player 1 Wins !!!!!!!!!!");
+			break;
+		}
 
 		if(DEBUG){
 			printf("Ai 2");
 			debugState(player2,ai2);
 		}
 
-		printf("Ai 2 Is Thinking ... ");
-		aiPlayer(player2, &ai2, shot);
-		printf("%c%d\n",shot[0]+97,shot[1]);
+		if(gameMode>=2){
+			printf("Enter Firing Coordinates (Letter Number):\n");
+			do{
+				char r;
+				scanf("%c%d", &r,&shot[1]);
+				shot[0]=tolower(r)-97;
+				if(!isValidRC(shot[0], shot[1])){
+					fputs("Coordinates Invalid\n",stdout);
+				}
+			}while(!isValidRC(shot[0], shot[1]));
+		}else{
+			printf("Ai 2 Is Thinking ... ");
+			aiPlayer(player2, &ai2, shot);
+			printf("%c%d\n",shot[0]+97,shot[1]);
+		}
 
 		//Shot Logic Player2 or AI
 		if(isHit(player1.board, shot[0], shot[1])){
@@ -89,13 +115,58 @@ void launchBattlehip(){
 			player2.shots[shot[0]][shot[1]]=miss;
 			player2.ShotHit=false;
 		}
+		shipSunk(&player2, &player1, shot);
+		if((gameEnd=isGameOver(player1.board, gameOverState1))){
+			printf("Player 2 Wins !!!!!!!!!!");
+			break;
+		}
 
 		printf("Press Any Key to Start Next Turn");
 		getchar();
 	}
 
 
-	exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
+}
+
+static int sGameOver(int arr[10][10]){
+	int state=0;
+	for(int r=0;r<boardrows;r++){
+		for(int c=0;c<boardcolumns;c++){
+			state+=-1*abs(arr[r][c]);
+		}
+	}
+	return state;
+}
+
+static bool isGameOver(int arr[10][10],int gameOverState){
+	int sum;
+	for(int r=0;r<boardrows;r++){
+		for(int c=0;c<boardcolumns;c++){
+			sum+=arr[r][c];
+		}
+	}
+	if(sum==gameOverState) return true;
+	return false;
+}
+
+static void shipSunk(board *player1,board *player2, int shot[2]){
+
+	if(player1->ShotHit==true){
+		int test = abs(player2->board[shot[0]][shot[1]]), accumelator=0;
+
+		for(int r=0;r<boardrows;r++){
+			for(int c=0;c<boardcolumns;c++){
+				if(player2->board[r][c]==test)
+					accumelator++;
+			}
+		}
+		if(accumelator==0){
+			player1->ShipSunk=true;
+		}
+	}else{
+		player1->ShipSunk=false;
+	}
 }
 
 static bool isValidRC(int r,int c){
@@ -121,7 +192,7 @@ static void debugState(board Board,aiMemory ai){
 		for(int c=0;c<10;c++){
 			if(Board.shots[r][c]>=0){
 				if(Board.shots[r][c]==0){
-					printf("  ");
+					printf(" .");
 				}
 				else printf(" %d",Board.shots[r][c]);
 			} else {
@@ -132,7 +203,7 @@ static void debugState(board Board,aiMemory ai){
 		for(int c=0;c<10;c++){
 			if(Board.board[r][c]>=0){
 				if(Board.board[r][c]==0){
-					printf("  ");
+					printf(" .");
 				}
 				else printf(" %d",Board.board[r][c]);
 			} else {
@@ -152,6 +223,12 @@ static void debugState(board Board,aiMemory ai){
 			break;
 		case 4:
 			printf(" SSS: %d %d",ai.secondShot[0],ai.secondShot[1]);
+			break;
+		case 5:
+			printf(" Hit: %d",Board.ShotHit);
+			break;
+		case 6:
+			printf(" Sunk: %d",Board.ShipSunk);
 			break;
 		}
 		printf("\n");
